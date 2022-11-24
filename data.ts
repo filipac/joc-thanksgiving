dd7f124ea35f8f49017c9c59007b214743f8e982
Filip Pacurar <filip@pacurar.net>
2022-11-25 00:08:20 +0200

    fix

--- data.ts
+++ data.ts
@@ -14,70 +14,70 @@ export const persoane = [
         image: 'p1.jpg',
         name: 'Thomas Fabian',
     },
-    // {
-    //     image: 'p2.jpg',
-    //     name: 'Miriam Fabian',
-    // },
-    // {
-    //     image: 'p3.jpg',
-    //     name: 'Dani Cosmescu',
-    // },
-    // {
-    //     image: 'p4.jpg',
-    //     name: 'Mali Cosmescu',
-    // },
-    // {
-    //     image: 'p5.jpg',
-    //     name: 'Darius Rusu',
-    // },
-    // {
-    //     image: 'p6.jpg',
-    //     name: 'Doariana Minzat',
-    // },
+    {
+        image: 'p2.jpg',
+        name: 'Miriam Fabian',
+    },
+    {
+        image: 'p3.jpg',
+        name: 'Dani Cosmescu',
+    },
+    {
+        image: 'p4.jpg',
+        name: 'Mali Cosmescu',
+    },
+    {
+        image: 'p5.jpg',
+        name: 'Darius Rusu',
+    },
+    {
+        image: 'p6.jpg',
+        name: 'Doariana Minzat',
+    },
     {
         image: 'p7.jpg',
         name: 'Liam Minzat',
     },
-    // {
-    //     image: 'p8.jpg',
-    //     name: 'Oty Minzat',
-    // },
-    // {
-    //     image: 'p9.jpg',
-    //     name: 'Beni Fabian',
-    // },
-    // {
-    //     image: 'p10.jpg',
-    //     name: 'Nati Hedean',
-    // },
-    // {
-    //     image: 'p11.jpg',
-    //     name: 'Alina Hedean',
-    // },
-    // {
-    //     image: 'p12.jpg',
-    //     name: 'Cristina Hedean',
-    // },
-    // {
-    //     image: 'p13.jpg',
-    //     name: 'Marius Hedean',
-    // },
-    // {
-    //     image: 'p14.jpg',
-    //     name: 'Daria Rusu',
-    // },
-    // {
-    //     image: 'p15.jpg',
-    //     name: 'Catalina Pacurar',
-    // },
+    {
+        image: 'p8.jpg',
+        name: 'Oty Minzat',
+    },
+    {
+        image: 'p9.jpg',
+        name: 'Beni Fabian',
+    },
+    {
+        image: 'p10.jpg',
+        name: 'Nati Hedean',
+    },
+    {
+        image: 'p11.jpg',
+        name: 'Alina Hedean',
+    },
+    {
+        image: 'p12.jpg',
+        name: 'Cristina Hedean',
+    },
+    {
+        image: 'p13.jpg',
+        name: 'Marius Hedean',
+    },
+    {
+        image: 'p14.jpg',
+        name: 'Daria Rusu',
+    },
+    {
+        image: 'p15.jpg',
+        name: 'Catalina Pacurar',
+    },
     {
         image: 'p16.jpg',
         name: 'Iosua Pacurar',
     },
-    // {
-    //     image: 'p17.jpg',
-    //     name: 'Filip Pacurar',
-    // },
+    {
+        image: 'p17.jpg',
+        name: 'Filip Pacurar',
+    },
     {
         image: 'p18.jpg',
         name: 'Caleb Pacurar',
